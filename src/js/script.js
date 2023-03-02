a94b6c0c9557e07f8305b31d124e0f34f71bb881
Albert Czarnecki <abath88@gmail.com>
2023-03-02 16:19:23 +0100

    Add price logic

--- src/js/script.js
+++ src/js/script.js
@@ -60,9 +60,20 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
+    }
 
-      console.log('new Product ', thisProduct);
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
     }
 
     renderInMenu(){
@@ -83,14 +94,12 @@
 
     initAccordion(){
       const thisProduct = this;
-      /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-  
+
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function(event) {
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
         event.preventDefault();
-        
+
         /* find active product (product that has active class) */
         const activeProduct = document.querySelector(select.all.menuProductsActive);
 
@@ -98,9 +107,51 @@
         if(activeProduct && thisProduct.element != activeProduct) {
           activeProduct.classList.toggle('active');
         }
-          /* toggle active class on thisProduct.element */
-          thisProduct.element.classList.toggle('active');
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
       });
+    }
+    initOrderForm(){
+      const thisProduct = this;
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+    processOrder(){
+      const thisProduct = this;
+      const formData = utils.serializeFormToObject(thisProduct.form);
+
+      let price = thisProduct.data.price;
+      for(let paramId in thisProduct.data.params){
+        const param = thisProduct.data.params[paramId];
+
+        for(let optionId in param.options){
+          const option = param.options[optionId];
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            if(!option.default ) {
+              price += option.price;
+            }
+          } else {
+            if(option.default) {
+              price -= option.price;
+            }
+          }
+        }
+      }
+
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
@@ -108,7 +159,6 @@
   const app = {
     initMenu: function(){
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
 
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
