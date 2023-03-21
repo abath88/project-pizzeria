99093edde9901deb471c3e41e48f85a0141976ad
Albert Czarnecki <abath88@gmail.com>
2023-03-21 15:00:55 +0100

    Add booking class

--- src/js/utils.js
+++ src/js/utils.js
@@ -7,13 +7,11 @@ utils.createDOMFromHTML = function(htmlString) {
   div.innerHTML = htmlString.trim();
   return div.firstChild;
 };
-
 utils.createPropIfUndefined = function(obj, key, value = []){
   if(!obj.hasOwnProperty(key)){
     obj[key] = value;
   }
 };
-
 utils.serializeFormToObject = function(form){
   let output = {};
   if (typeof form == 'object' && form.nodeName == 'FORM') {
@@ -35,7 +33,6 @@ utils.serializeFormToObject = function(form){
   }
   return output;
 };
-
 utils.convertDataSourceToDbJson = function(){
   const productJson = [];
   for(let key in dataSource.products){
@@ -43,6 +40,27 @@ utils.convertDataSourceToDbJson = function(){
   }
 
   console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
+};
+utils.queryParams = function(params){
+  return Object.keys(params)
+    .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+    .join('&');
+};
+utils.numberToHour = function(number){
+  return (Math.floor(number) % 24) + ':' + (number % 1 * 60 + '').padStart(2, '0');
+};
+utils.hourToNumber = function(hour){
+  const parts = hour.split(':');
+
+  return parseInt(parts[0]) + parseInt(parts[1])/60;
+};
+utils.dateToStr = function(dateObj){
+  return dateObj.toISOString().slice(0, 10);
+};
+utils.addDays = function(dateStr, days){
+  const dateObj = new Date(dateStr);
+  dateObj.setDate(dateObj.getDate() + days);
+  return dateObj;
 };
 
 Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
